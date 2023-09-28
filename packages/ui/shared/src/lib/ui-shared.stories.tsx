a6b68fcdf6514e4d67cdccfde9eac8c548ce087b
Oskar Hulter <stresslessness91@gmal.com>
2023-09-28 22:18:31 +0200

    add storybook

---
+++ packages/ui/shared/src/lib/ui-shared.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { UiShared } from './ui-shared';
+
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta: Meta<typeof UiShared> = {
+  component: UiShared,
+  title: 'UiShared',
+};
+export default meta;
+type Story = StoryObj<typeof UiShared>;
+
+export const Primary = {
+  args: {},
+};
+
+export const Heading: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/Welcome to UiShared!/gi)).toBeTruthy();
+  },
+};
