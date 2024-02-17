eea9555c0d12577a63725b214397373913014275
Oskar Hulter <stresslessness91@gmail.com>
2024-02-17 01:27:58 +0100

    update deps

--- packages/ui/email/src/lib/example-email/exampleEmail.tsx
+++ packages/ui/email/src/lib/example-email/exampleEmail.tsx
@@ -1,18 +1,15 @@
-import { Button } from '@react-email/button';
-import { Html } from '@react-email/html';
-import * as React from 'react';
+import { Button } from '@react-email/button'
+import { Html } from '@react-email/html'
 
 export default function ExampleEmail() {
   return (
     <Html>
       <Button
-        pX={20}
-        pY={12}
         href='https://example.com'
         style={{ background: '#000', color: '#fff' }}
       >
         Click me
       </Button>
     </Html>
-  );
+  )
 }
